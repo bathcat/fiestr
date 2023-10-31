c12dba60db12b9dc49e57363d341e9a26c275c10
bathcat <eric.mcmullen@gmail.com>
2023-10-31 09:33:23 -0600

    Refactor fizzbuzz lab.

--- src/practice/fizz-buzz/fizz-buzz-practice.tsx
+++ src/practice/fizz-buzz/fizz-buzz-practice.tsx
@@ -1,4 +1,25 @@
 import { ReadMe } from '@components/read-me';
-import readme from './README.md?raw';
+import { FizzBuzz as FizzBuzzStarter } from './starter/fizz-buzz';
+import { FizzBuzz as FizzBuzzComplete } from './complete/fizz-buzz';
 
-export const FizzBuzzPractice = () => <ReadMe markdown={readme} />;
+import readme from './README.md?raw';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+export const FizzBuzzPractice = () => (
+  <Tabs defaultValue="overview">
+    <TabsList>
+      <TabsTrigger value="overview">Overview</TabsTrigger>
+      <TabsTrigger value="starter">Starter</TabsTrigger>
+      <TabsTrigger value="complete">Complete</TabsTrigger>
+    </TabsList>
+    <TabsContent value="overview">
+      <ReadMe markdown={readme} />
+    </TabsContent>
+    <TabsContent value="starter">
+      <FizzBuzzStarter />
+    </TabsContent>
+    <TabsContent value="complete">
+      <FizzBuzzComplete />
+    </TabsContent>
+  </Tabs>
+);
